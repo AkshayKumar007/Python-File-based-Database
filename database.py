4afcdeba3e89cd53d82d02b8301b0c2b5ee219b4
AkshayKumar007 <akshaykrcc@live.com>
2021-01-01 17:24:18 +0530

    added code for time to live

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from genericpath import exists
 import os
+import datetime
 import json
 import fcntl  # for UNIX only
 import atexit
@@ -7,7 +8,8 @@ import atexit
 from os import path
 from threading import Thread
 
-
+key_type_error = TypeError('Key Should be a string')
+key_exists_error = IndexError('Key already exists')
 key_char_limit_error = TypeError('Key length shall not exceed 32 characters')
 json_size_limit_error = MemoryError('JSON size exceeded 16KB')
 db_size_limit_error = MemoryError('File size reached 1GB. Try creating a new Database instance')
@@ -19,7 +21,6 @@ class Database:
         self.filename = filepath
         
         self.file_obj = open(filepath, 'r+')
-        print('yes')
         self.file_no = self.file_obj.fileno()
         
         try:
@@ -28,7 +29,7 @@ class Database:
         except OSError as e:
             raise PermissionError('File is being used by other process(es)')
 
-    def load_db(self): # opens file containg db
+    def load_db(self):  # opens file containg db
         try:
             self.db = json.load(self.file_obj)   
         except ValueError as e:
@@ -37,7 +38,7 @@ class Database:
             else:
                 raise ValueError('File is corrupted or not in JSON format.')
     
-    def close_db(self): # for closing file and removing lock on file by process
+    def close_db(self):  # for closing file and removing lock on file by process
         if hasattr(self, 'db'):
             del self.db
         fcntl.lockf(self.file_no, fcntl.LOCK_UN)
@@ -53,30 +54,50 @@ class Database:
         return self.db
     
     def read_by_key(self, key):
+
         return self.db[key]
 
-    def bulk_write(self, liz):
+    def bulk_write(self, liz, time_to_live=None):
         for index in range(len(liz)):
             for key in liz[index]:
-                self.db[key] = liz[index][key]
+                self.write(key, liz[index][key], time_to_live)
     
     def write(self, key, value):
-        json_obj = json.dumps(value)
-        json_size = len(json_obj.encode('utf-8'))
-        if type(key) != str:
-            raise TypeError('Key Should be a string')
-        elif len(key) > 32:
-            raise key_char_limit_error
-        elif json_size > 16000:
-            raise json_size_limit_error
+        choice = input('Include Time to Live(Y/n)?')
+        if choice == 'Y' or 'y':
+            date_entry = input('Enter a date in YYYY-MM-DD-HH-MM format:\t')
+            time_to_live = datetime.datetime.strptime(date_entry, '%Y-%m-%d-%H-%M')
+            epoch = datetime.datetime.utcfromtimestamp(0)
+            time_to_live = (time_to_live - epoch).total_seconds()
         else:
-            self.db[key] = value
+            time_to_live = None
+        try:
+            if type(key) != str:
+                raise key_type_error
+            elif len(key) > 32:
+                raise key_char_limit_error
+            elif self.db.get(key, None) != None:
+                raise key_exists_error
+            else:
+                if time_to_live != None:
+                    value = {'time-to-live' : time_to_live, 'value' : value}
+                else:
+                    value = {'value' : value}
+                json_obj = json.dumps(value)
+                json_size = len(json_obj.encode('utf-8'))
+                if json_size > 16384:
+                    raise json_size_limit_error
+                else:
+                    self.db[key] = json_obj
+
+        except:
+            raise TypeError('Object not serializable')
 
     def delete_by_key(self, key):
         del self.db[key]
         
     def delete_db(self):
-        del self.db
+        self.db = {}
 
 def create_db(dir_path=None):
     
---
+++ tests.py
@@ -0,0 +1,116 @@
+import database
+
+# a work in progress
+
+
+class TestClass(object):
+
+    db = database.load('tests.db', auto_dump=False)
+
+    def test_load(self):
+        x = database.load('x.db', auto_dump=False)
+        assert x is not None
+
+    def test_sugar_get(self):
+        self.db.db["foo"] = "bar"
+        x = self.db["foo"]
+        assert x == "bar"
+
+    def test_sugar_set(self):
+        self.db["foo"] = "bar"
+        assert "bar" == self.db.db["foo"]
+
+    def test_sugar_rem(self):
+        self.db.db["foo"] = "bar"
+        del self.db["foo"]
+        assert "foo" not in self.db.db
+
+    def test_set(self):
+        self.db.set('key', 'value')
+        x = self.db.get('key')
+        assert x == 'value'
+
+    def test_getall(self):
+        self.db.deldb()
+        self.db.set('key1', 'value1')
+        self.db.set('key2', 'value2')
+        self.db.dcreate('dict1')
+        self.db.lcreate('list1')
+        x = self.db.getall()
+        y = dict.fromkeys(['key2', 'key1', 'dict1', 'list1']).keys()
+        assert x == y
+
+    def test_get(self):
+        self.db.set('key', 'value')
+        x = self.db.get('key')
+        assert x == 'value'
+
+    def test_rem(self):
+        self.db.set('key', 'value')
+        self.db.rem('key')
+        x = self.db.get('key')
+        assert x is False
+
+    def test_append(self):
+        self.db.set('key', 'value')
+        self.db.append('key', 'value')
+        x = self.db.get('key')
+        assert x == 'valuevalue'
+
+    def test_exists(self):
+        self.db.set('key', 'value')
+        x = self.db.exists('key')
+        assert x is True
+        self.db.rem('key')
+
+    def test_not_exists(self):
+        self.db.set('key', 'value')
+        x = self.db.exists('not_key')
+        assert x is False
+        self.db.rem('key')
+
+    def test_lexists(self):
+        self.db.lcreate('list')
+        self.db.ladd('list', 'value')
+        x = self.db.lexists('list', 'value')
+        assert x is True
+        self.db.lremlist('list')
+
+    def test_not_lexists(self):
+        self.db.lcreate('list')
+        self.db.ladd('list', 'value')
+        x = self.db.lexists('list', 'not_value')
+        assert x is False
+        self.db.lremlist('list')
+
+    def test_lrange(self):
+        self.db.lcreate('list')
+        self.db.ladd('list','one')
+        self.db.ladd('list','two')
+        self.db.ladd('list','three')
+        self.db.ladd('list','four')
+        x = self.db.lrange('list', 1, 3)
+        assert x == ['two', 'three']
+        self.db.lremlist('list')
+
+    def test_dexists(self):
+        self.db.dcreate('dict')
+        self.db.dadd('dict', ('key', 'value'))
+        x = self.db.dexists('dict', 'key')
+        assert x is True
+        self.db.drem('dict')
+
+    def test_not_dexists(self):
+        self.db.dcreate('dict')
+        self.db.dadd('dict', ('key', 'value'))
+        x = self.db.dexists('dict', 'not_key')
+        assert x is False
+        self.db.drem('dict')
+
+
+if __name__ == "__main__":
+    tests = TestClass()
+    test_methods = [method for method in dir(tests) if callable(getattr(tests, method)) if method.startswith('test_')]
+    for method in test_methods:
+            getattr(tests, method)()  # run method
+            print(".", end="")
